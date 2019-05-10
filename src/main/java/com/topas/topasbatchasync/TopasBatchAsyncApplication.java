cb634f103872fd2188671168bacea5a75252db60
lsv40 <lsv40@DESKTOP-62E0335.mshome.net>
2019-05-10 20:49:37 +0900

    local project

---
+++ src/main/java/com/topas/topasbatchasync/TopasBatchAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.topas.topasbatchasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopasBatchAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopasBatchAsyncApplication.class, args);
+	}
+
+}
